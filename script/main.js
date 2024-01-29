06ba47eaa1f0b834058baf8f72daa7d21aa1af6f
pdh02189 <pdh02189@naver.com>
2024-01-29 13:12:40 +0900

    js수정
    수정완료

--- script/main.js
+++ script/main.js
@@ -27,10 +27,23 @@ dday_form.addEventListener("submit", function(e) {
     const name2 = document.querySelector("#name2").value;
     const nameField1 = document.querySelector(".name1");
     const nameField2 = document.querySelector(".name2");
+    const firstDate = document.querySelector("#firstDate").value;
+    const inputDay = document.querySelector(".inputDay");
+    const special1 = document.querySelector(".countDay1 span");
+    const special2 = document.querySelector(".countDay2 span");
+
+
     nameField1.innerText = name1;
     nameField2.innerText = name2;
+    inputDay.innerText = firstDate;
+
+    let afterDay1 = theDay.setDate(theDay.getDate() + 300);
+    let afterDay2 = theDay.setFullYear(theDay.getDate() + 1);
+    special1.innerText = afterDay1.toLocaleString();
+    special2.innerText = afterDay2.toLocaleString();
+    console.log(afterDay1);
+    console.log(afterDay2);
 
     resultDiv.classList.remove("d_none");
-
 
 });
